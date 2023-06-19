2f8def7de7a3046fe3ba2781e1e9c08991843c46
nicfer21 <ferboncun@gmail.com>
2023-06-19 17:11:08 -0500

    Procedure

--- controller/maintenanceprocedure.controller.js
+++ controller/maintenanceprocedure.controller.js
@@ -1,4 +1,5 @@
 import MaintenanceProcedureModel from "../models/maintenanceprocedure.model.js";
+import db from "../database/mantenimiento.db.js";
 
 export const getAllMaintenanceProcedure = async (req, res) => {
   try {
@@ -13,14 +14,25 @@ export const getAllMaintenanceProcedure = async (req, res) => {
   }
 };
 
+export const getMaintenanceProcedureByMachine = async (req, res) => {
+  try {
+    const rs = await db.query(
+      `select * from view_maintenanceprocedure_data v where v.idMachine = '${req.params.id}'  ;`
+    );
+    res.json(rs[0]);
+  } catch (error) {
+    res.json({
+      error: error,
+    });
+  }
+};
+
 export const getOneMaintenanceProcedure = async (req, res) => {
   try {
-    const rs = await MaintenanceProcedureModel.findOne({
-      where: {
-        idMaintenanceProcedure: req.params.id,
-      },
-    });
-    res.json(rs);
+    const rs = await db.query(
+      `select * from view_maintenanceprocedure_data v where v.idMaintenanceProcedure = '${req.params.id}'  ;`
+    );
+    res.json(rs[0][0]);
   } catch (error) {
     res.json({
       error: error,
--- routes/maintenanceprocedure.route.js
+++ routes/maintenanceprocedure.route.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {
   getAllMaintenanceProcedure,
+  getMaintenanceProcedureByMachine,
   getOneMaintenanceProcedure,
 } from "../controller/maintenanceprocedure.controller.js";
 
 const router = express.Router();
 
 router.get("/", getAllMaintenanceProcedure);
-router.get("/:id", getOneMaintenanceProcedure);
+router.get("/a/:id", getMaintenanceProcedureByMachine);
+router.get("/b/:id", getOneMaintenanceProcedure);
 
 export default router;
